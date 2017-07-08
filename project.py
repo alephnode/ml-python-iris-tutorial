e2f339e71516303cb7ba79afb87ecf12e885c53d
Stephen Ward <srmward@gmail.com>
2017-07-08 11:44:44 -0700

    added histogram and multivariate plotting ability

--- project.py
+++ project.py
@@ -25,3 +25,11 @@ print(dataset.groupby('class').size())
 # box and whisker plots
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
 plt.show()
+
+# histograms
+dataset.hist()
+plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
